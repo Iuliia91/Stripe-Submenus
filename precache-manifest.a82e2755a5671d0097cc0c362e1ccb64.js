1f51c417eaeb509511444dcca959fdecde426924
oleksandrprusakov <aleksandrprusakov48@gmail.com>
2022-11-20 12:23:00 +0200

    Updates

---
+++ precache-manifest.a82e2755a5671d0097cc0c362e1ccb64.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e3c845e4671bde1db2e390969b56ef",
+    "url": "/Stripe-Submenus/index.html"
+  },
+  {
+    "revision": "84b205bfc0413ae01a3d",
+    "url": "/Stripe-Submenus/static/css/main.c0b00f0e.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/Stripe-Submenus/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stripe-Submenus/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b205bfc0413ae01a3d",
+    "url": "/Stripe-Submenus/static/js/main.bc9c3d5d.chunk.js"
+  },
+  {
+    "revision": "9b471be1dcccab7eb45f",
+    "url": "/Stripe-Submenus/static/js/runtime-main.531f0737.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/Stripe-Submenus/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/Stripe-Submenus/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/Stripe-Submenus/static/media/phone.58d7e3d6.svg"
+  }
+]);
